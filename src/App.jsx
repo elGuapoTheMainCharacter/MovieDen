db76e306702d4a6eb5b6dc9697bb18dc12a42e5a
elGuapoTheMainCharacter <168385602+elGuapoTheMainCharacter@users.noreply.github.com>
2026-02-21 18:56:01 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import MovieCard from "./components/MovieCard"
+import Home from "./pages/Home"
+import {Routes,Route} from "react-router-dom"
+import "./css/App.css"
+import Favorites from "./pages/Favorites"
+import {MovieProvider} from "./contexts/MovieContext"
+import NavBar from "./components/NavBar"
+export default function App(){
+  return(
+    <MovieProvider>
+      <NavBar></NavBar>
+      <main clasName = "main-content">
+        <Routes>
+          <Route path = "/" element = {<Home></Home>}></Route>
+          <Route path = "/favorites" element = {<Favorites></Favorites>}></Route>
+        </Routes>
+      </main>
+    </MovieProvider>
+  )
+}
