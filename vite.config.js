c1fc2cef6ce199f03ab02a59386d1a6edd968ecf
elGuapoTheMainCharacter <168385602+elGuapoTheMainCharacter@users.noreply.github.com>
2026-02-21 19:43:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/your-repo-name/', // Must match your repo name exactly
+  base: '/MovieDen/', // Must match your repo name exactly
   plugins: [react()],
 })
 
