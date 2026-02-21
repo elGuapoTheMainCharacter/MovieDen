edf85aea587f3a8e429b497f86b4211253d84577
elGuapoTheMainCharacter <168385602+elGuapoTheMainCharacter@users.noreply.github.com>
2026-02-21 18:54:57 +0200

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "../css/Footer.css"
+function Footer(){
+    return(
+        <footer>
+            <p> <strong>&copy; ZS NKEFE 2026</strong></p>
+        </footer>
+    )
+}
+export default Footer;
