f64422aed2361c41c660145413d3bca29de70a7d
elGuapoTheMainCharacter <168385602+elGuapoTheMainCharacter@users.noreply.github.com>
2026-02-21 19:42:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  base: '/your-repo-name/', // Must match your repo name exactly
+  plugins: [react()],
+})
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
