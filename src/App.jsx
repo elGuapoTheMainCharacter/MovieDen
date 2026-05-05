0d76584edd4d6c718f31c6162c65e1f768cd8cf7
elGuapoTheMainCharacter <168385602+elGuapoTheMainCharacter@users.noreply.github.com>
2026-05-05 17:53:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import "./css/App.css"
 import Favorites from "./pages/Favorites"
 import {MovieProvider} from "./contexts/MovieContext"
 import NavBar from "./components/NavBar"
-export default function App(){
-  return(
+export default function App() {
+  return (
     <MovieProvider>
-      <NavBar></NavBar>
-      <main className = "main-content">
+      <NavBar />
+      {/* FIX: Change 'clasName' to 'className' */}
+      <main className="main-content"> 
         <Routes>
-          <Route path = "/" element = {<Home></Home>}></Route>
-          <Route path = "/favorites" element = {<Favorites></Favorites>}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
     </MovieProvider>
-  )
+  );
 }
+
